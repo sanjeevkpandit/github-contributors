c4e350d35fee3e68cbf5af60ee87e160b2de0608
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2017-09-06 13:17:23 +0545

    Add footer and github link in footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import NavBar from './NavBar';
+import Footer from './Footer';
 import ContributorsList from './ContributorsList';
 
 const App = () => (
   <div>
     <NavBar />
     <ContributorsList repo="facebook/react" />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import gitHubLogo from '../assets/images/GitHub-Mark.png';
+
+const Footer = () => (
+  <footer>
+    <hr/>
+
+    <div className="container">
+      <p>
+        &copy; 2017
+        <span className="float-right">
+          <a href="https://github.com/sanjeevkpandit/github-contributors" target="_blank">
+            <img src={gitHubLogo} width="30" alt="GitHub Contributors"/>
+          </a>
+        </span>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
