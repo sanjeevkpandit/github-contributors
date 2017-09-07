9c114d5d5f4a64e3b5cba7ed6c64cded34ed91bb
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2017-09-07 17:33:02 +0545

    Fix the position of footer to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,13 @@ import gitHubLogo from '../assets/images/GitHub-Mark.png';
 
 const Footer = () => (
   <footer className="footer">
-    <hr/>
-
     <div className="container">
-      <p>
-        &copy; 2017
-        <span className="float-right">
-          <a href="https://github.com/sanjeevkpandit/github-contributors" target="_blank" rel="noopener noreferrer">
-            <img src={gitHubLogo} width="30" alt="GitHub Contributors"/>
-          </a>
-        </span>
-      </p>
+      &copy; 2017
+      <span className="float-right">
+        <a href="https://github.com/sanjeevkpandit/github-contributors" target="_blank" rel="noopener noreferrer">
+          <img src={gitHubLogo} width="24" alt="GitHub Contributors"/>
+        </a>
+      </span>
     </div>
   </footer>
 );
