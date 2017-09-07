78cdd55bd2c9895b80c3812f8eec23954277942e
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2017-09-07 13:12:47 +0545

    Make profile image circular

--- src/components/Contributor.js
+++ src/components/Contributor.js
@@ -4,8 +4,10 @@ const Contributor = ({contributor}) => {
   return (
     <div className="col-sm-2">
       <div className="card">
-        <img className="card-img-top contributor-image" src={contributor.avatar_url} alt={contributor.login}/>
-        <div className="card-block">
+        <a href={contributor.html_url} target="_blank">
+          <img className="card-img-top contributor-image" src={contributor.avatar_url} alt={contributor.login}/>
+        </a>
+        <div className="card-block text-center">
           <a className="username" href={contributor.html_url} target="_blank">{contributor.login}</a>
           <p className="card-text">
             <small>{contributor.contributions} commits</small>
