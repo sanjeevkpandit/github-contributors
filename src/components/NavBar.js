192fbd86e27bd669f39a4038757371e5652e555f
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-10-05 19:01:35 +0545

    Remove unnecessary definitions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import gitHubLogo from '../assets/images/GitHub-Mark.png';
-
 const NavBar = ({handleToggleSearchResults, searchKey}) => (
   <nav>
     <h1 className="display-4 text-center logo">
       <a href="/github-contributors">
-        {/* <img src={gitHubLogo} alt="GitHub Contributors"/> */}
         GitHub Contributors
       </a>
     </h1>
