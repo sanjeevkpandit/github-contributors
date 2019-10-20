175cc2809f7e3c49ea91ce63d715d5066ac89859
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2019-10-20 22:10:51 +0545

    Updates

---
+++ precache-manifest.4031047007ca84dbecdef7a008bc7d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f64e5dca662a93e2cbbc0d1eb2151ca",
+    "url": "/github-contributors/index.html"
+  },
+  {
+    "revision": "ceea93652c273b008695",
+    "url": "/github-contributors/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "e282425a8d637c342d9b",
+    "url": "/github-contributors/static/css/main.939adc35.chunk.css"
+  },
+  {
+    "revision": "ceea93652c273b008695",
+    "url": "/github-contributors/static/js/2.20656893.chunk.js"
+  },
+  {
+    "revision": "e282425a8d637c342d9b",
+    "url": "/github-contributors/static/js/main.32c1dbe9.chunk.js"
+  },
+  {
+    "revision": "2bba06c0d302715185f8",
+    "url": "/github-contributors/static/js/runtime-main.6176c826.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-contributors/precache-manifest.11d4cb698398fab5683ffcc78ce24717.js"
+  "/github-contributors/precache-manifest.4031047007ca84dbecdef7a008bc7d71.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github-contributors/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-contributors/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
